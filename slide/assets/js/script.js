c3d27d80f523652efd6e3a6a61c675f763c50c48
NamanGoyal-415 <139861180+NamanGoyal-415@users.noreply.github.com>
2023-08-14 14:35:15 +0530

    Add files via upload

---
+++ slide/assets/js/script.js
@@ -0,0 +1,11 @@
+$('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    Infinity:true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: true,
+    prevArrow: '.prev',
+    nextArrow: '.next'
+});
